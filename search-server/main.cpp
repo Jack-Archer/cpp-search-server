9fbb36c29d657a6fbe1833fdf98bd9a5bc15c535
Jack-Archer <114923937+Jack-Archer@users.noreply.github.com>
2023-03-06 00:39:29 +0300

    6 sprint review

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -35,7 +35,7 @@ int main() {
     // слова из разных документов, не является дубликатом
     search_server.AddDocument(9, "nasty rat with curly hair"s, DocumentStatus::ACTUAL, {1, 2});
     
-    
+
     cout << "Before duplicates removed: "s << search_server.GetDocumentCount() << endl;
     RemoveDuplicates(search_server);
     cout << "After duplicates removed: "s << search_server.GetDocumentCount() << endl;
